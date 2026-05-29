c1c5ebc7c521c6d9f0ee61af47b26cceffa8cdea
24362-Jenny <24362@burnside.school.nz>
2026-05-29 12:41:37 +1200

    bullet code for enemy

---
+++ scripts/bullet_1.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+var speed = 300
+var direction : Vector2 = Vector2.UP
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	translate(direction * speed * delta)
