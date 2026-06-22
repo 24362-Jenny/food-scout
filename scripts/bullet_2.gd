4495a1d7ef277409cdda3a1942e4197e466166e6
24362-Jenny <24362@burnside.school.nz>
2026-06-23 09:25:20 +1200

    second enemy done

--- scripts/bullet_2.gd
+++ scripts/bullet_2.gd
@@ -7,6 +7,9 @@ var direction : Vector2 = Vector2.LEFT
 func _ready() -> void:
 	add_to_group("bullet")
 
+var fire_cooldown = 5.0 
+var time_since_last_shot = 0.0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/shooter.gd
+++ scripts/shooter.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var bullet_prefab = preload("res://scenes/bullet_1.tscn")
 
-
 func _on_timer_timeout() -> void:
 	#spawn a bullet
 	var bullet = bullet_prefab.instantiate()
