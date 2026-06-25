75dfc25aaaa75561e4309e91847aef849497b2c8
24362-Jenny <24362@burnside.school.nz>
2026-06-25 14:40:09 +1200

    ended with a mistake

--- scripts/bullet_2.gd
+++ scripts/bullet_2.gd
@@ -1,16 +1,20 @@
 extends Area2D
 class_name Bullet
 
+func _ready():
+	# Wait for 3.0 seconds, then delete this bullet
+	await get_tree().create_timer(3.0).timeout
+	queue_free()
+
+func _process(delta):
+	# Move the bullet forward
+	position += transform.x * speed * delta
+
 var speed = 300
 var direction : Vector2 = Vector2.LEFT
 
 func _ready() -> void:
 	add_to_group("bullet")
 
-var fire_cooldown = 5.0 
+var fire_cooldown = 5.0 
 var time_since_last_shot = 0.0
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	translate(direction * speed * delta)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,28 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -500.0
 var start_position = Vector2(584,400)
 
+@export var MAX_JUMPS: int = 2
+var jump_count: int = 0
 
 func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		# get_gravity() returns a Vector2, so we multiply by delta and add to velocity
+		velocity += get_gravity() * delta
+	else:
+		# Reset jump count when landing on the ground
+		jump_count = 0
+
+	# Handle jump & double jump. 
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or jump_count < MAX_JUMPS:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -36,5 +53,3 @@ func _on_hitbox_area_entered(_area: Area2D) -> void:
 	print(_area.get_groups())
 	if _area.is_in_group("bullet"):
 		get_tree().change_scene_to_file("res://scenes/death_scene.tscn")
-	
-	
