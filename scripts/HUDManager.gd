183a4b3804c89b636e66021284796a22d3bf08ef
24362-Jenny <24362@burnside.school.nz>
2026-07-03 09:24:59 +1200

    need to finish script

--- scripts/HUDManager.gd
+++ scripts/HUDManager.gd
@@ -2,18 +2,49 @@ extends Node
 
 @onready var time: Label = $"../CanvasLayer/Control/VBoxContainer/Time/Label"
 @onready var timer: Timer = $"../Timer"
+@onready var countdown_timer: Timer = $CountdownTimer
+@onready var timer_label: Label = $TimerLabel
+
+@export var score_multiplier: int = 100
+var final_score: int = 0
+var level_active: bool = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	time.text = str(int(timer.time_left))
+	# Start the countdown when the level begins
+	countdown_timer.start()
 
-
-func update_player_falls(amount: int):
-	StateOfHud.player_falls += amount
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	
 	time.text = str(int(timer.time_left))
+	
+	if not level_active and not countdown_timer.is_stopped():
+		#update the UI text to show whole seconds left
+		timer_label.text = "Time Left: " + str(ceil(countdown_timer.time_left))
+
+func _on_level_completed() -> void: 
+	if not level_active:
+		return
+	
+	level_active = false
+	countdown_timer.stop() #freeze the clock
+	
+	# Calculating score based on the remaining time
+	var time_left = countdown_timer.time_left
+	final_score = int(time_left * score_multiplier)
+	
+	get_tree().change_scene_to_file("res://scenes/ending_scene_level.tscn")
+
+
+
+
+
+
+
+func _on_countdown_timer_timeout() -> void:
+	pass # Replace with function body.
--- scripts/state_of_HUD.gd
+++ scripts/state_of_HUD.gd
@@ -3,7 +3,6 @@ extends Node
 
 var score: int = 0
 var time_taken: int = 0
-var player_falls: int = 0
 
 func add_score():
 	score += 1
@@ -11,4 +10,3 @@ func add_score():
 func reset():
 	score = 0
 	time_taken = 0
-	player_falls = 0
